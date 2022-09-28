969c0e775d013a9a5b691664c39948715e76a093
Fernando Almendro Martí <72808136+FernaToty@users.noreply.github.com>
2022-09-28 09:18:20 +0200

    Closea alchie

--- documents/ModuleInput.cpp
+++ documents/ModuleInput.cpp
@@ -1,6 +1,7 @@
 #include "Globals.h"
 #include "Application.h"
 #include "ModuleInput.h"
+#include "imgui_impl_sdl.h"
 
 #define MAX_KEYS 300
 
@@ -88,6 +89,8 @@ update_status ModuleInput::PreUpdate(float dt)
 	
 	while(SDL_PollEvent(&e))
 	{
+		
+		ImGui_ImplSDL2_ProcessEvent(&e);
 		switch(e.type)
 		{
 			case SDL_MOUSEWHEEL:
