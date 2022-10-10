4dca6bb06fcf6e52f3c009886f5677f3d9a87a15
Fernando Almendro Martí <fernandoalmendromarti@gmail.com>
2022-10-10 18:05:47 +0200

    A boat està completed ya

--- documents/ModuleScene_01.cpp
+++ documents/ModuleScene_01.cpp
@@ -324,18 +324,48 @@ update_status ModuleScene_01::menuDisplay()
         {
 
 
-            ImGui::Text("ABOUT THIS DEMO:");
-            ImGui::BulletText("BrumBrum Engine");
-            ImGui::BulletText("Best Motor ever not of a car");
-
-            ImGui::Text("PROGRAMMER GUIDE:");
-
-            ImGui::BulletText("Abochan & Juan Fernando (https://github.com/AlCh440/EngineBrumBrum)");
-            ImGui::BulletText("We have the Mathegeolib, the glew, the JSON and the SDL");
-            ImGui::BulletText("License");
+            ImGui::Text("Brenda 1:");
+            ImGui::Text("The future of the engines");
+            ImGui::Text("By Albert Chica & Fernando Almendro");
+            ImGui::Text("Github: https://github.com/AlCh440/EngineBrumBrum ");
             ImGui::Separator();
 
-            ImGui::Text("USER GUIDE:");
+            ImGui::Text("3D Party libraries used:");
+
+            
+            ImGui::BulletText("SDL");
+            ImGui::BulletText("GLEW");
+            ImGui::BulletText("MathGeoLib");
+            ImGui::BulletText("ImGui");
+            ImGui::BulletText("Parson");
+            ImGui::BulletText("Assimp");
+            ImGui::Separator();
+
+            ImGui::Text("License:");
+            ImGui::Text("MIT License");
+            ImGui::Text("Copyright (c) 2022 AlCh440");
+            
+            ImGui::Text("Permission is hereby granted, free of charge, to any person obtaining a copy");
+            ImGui::Text("of this software and associated documentation files(the Software), to deal");
+            ImGui::Text("in the Software without restriction, including without limitation the rights");
+            ImGui::Text("to use, copy, modify, merge, publish, distribute, sublicense, and /or sell");
+            ImGui::Text("copies of the Software, and to permit persons to whom the Software is");
+            ImGui::Text("furnished to do so, subject to the following conditions : ");
+           
+            ImGui::Text("The above copyright notice and this permission notice shall be included in all");
+            ImGui::Text("copies or substantial portions of the Software.");
+           
+            ImGui::Text("THE SOFTWARE IS PROVIDED AS IS, WITHOUT WARRANTY OF ANY KIND, EXPRESS OR");
+            ImGui::Text("IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,");
+            ImGui::Text("FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.IN NO EVENT SHALL THE");
+            ImGui::Text("AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER");
+            ImGui::Text("LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,");
+            ImGui::Text("OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE");
+            ImGui::Text("SOFTWARE.");
+
+            ImGui::Separator();
+
+            ImGui::Text("User's guide:");
             ImGui::ShowUserGuide();
         }
 
